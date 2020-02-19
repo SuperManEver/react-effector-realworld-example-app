c787b89062818baf6a52b7dd4ab84096e407285c
Nick Luparev <nikita.luparev@gmail.com>
2020-02-19 22:46:00 +0500

    login form with validation

---
+++ src/components/InputField.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useField } from 'formik'
+import styled from '@emotion/styled'
+
+type InputProps = {
+  name: string
+  type: 'email' | 'password'
+  label?: string
+  placeholder: string
+}
+
+const ErrorMsg = styled.div`
+  color: #d62701;
+  margin-top: 4px;
+`
+
+export const InputField = ({ label, name, type }: InputProps) => {
+  const props = { name, type }
+
+  const [field, meta] = useField(props)
+
+  console.log(field, meta)
+
+  return (
+    <>
+      <input className="form-control form-control-lg" {...field} {...props} />
+      {meta.touched && meta.error ? <ErrorMsg>{meta.error}</ErrorMsg> : null}
+    </>
+  )
+}
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,14 +1,62 @@
 import React from 'react'
-import { RouteComponentProps } from '@reach/router'
+import { RouteComponentProps, Link } from '@reach/router'
 
 import { WithHeader } from 'layout/WithHeader'
 
 type Props = RouteComponentProps
 
 export function Signup<Props>(props: Props) {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
   return (
     <WithHeader>
-      <div>Signup</div>
+      <div className="auth-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign In</h1>
+              <p className="text-xs-center">
+                <Link to="/register">Need an account?</Link>
+              </p>
+
+              {/* <ListErrors errors={this.props.errors} /> */}
+
+              <form>
+                <fieldset>
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      // onChange={this.changeEmail}
+                    />
+                  </fieldset>
+
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      // onChange={this.changePassword}
+                    />
+                  </fieldset>
+
+                  <button
+                    className="btn btn-lg btn-primary pull-xs-right"
+                    type="submit"
+                    // disabled={this.props.inProgress}
+                  >
+                    Sign in
+                  </button>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </WithHeader>
   )
 }
